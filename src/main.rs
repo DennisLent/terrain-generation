b5086b9a3f877d7b2cce3e239d83a2f5db62e938
DennisLent <dennis.lentschig955@gmail.com>
2025-02-05 20:51:21 +0100

    updated visualization to use bevy and reworked basic algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,91 @@
-use generation::Map;
-use render::render2d;
+mod generate_world;
+use bevy::{
+    color::palettes::css::*,
+    pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
+    prelude::*,
+    render::{render_resource::WgpuFeatures, settings::{RenderCreation, WgpuSettings}, RenderPlugin},
+};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use bevy::render::mesh::Mesh;
+use std::f32::consts::PI;
+use generate_world::generate_terrain_mesh;
 
-mod gen_utils;
-mod generation;
-mod render;
-mod river;
-mod simplex;
-mod sines;
 
 fn main() {
-    let map = Map::new(1);
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    // WARN this is a native only feature. It will not work with webgl or webgpu
+                    features: WgpuFeatures::POLYGON_MODE_LINE,
+                    ..default()
+                }),
+                ..default()
+            }),
+            // You need to add this plugin to enable wireframe rendering
+            WireframePlugin,
+            PanOrbitCameraPlugin,
+        ))
+        // Wireframes can be configured with this resource. This can be changed at runtime.
+        .insert_resource(WireframeConfig {
+            // The global wireframe config enables drawing of wireframes on every mesh,
+            // except those with `NoWireframe`. Meshes with `Wireframe` will always have a wireframe,
+            // regardless of the global configuration.
+            global: false,
+            // Controls the default color of all wireframes. Used as the default color for global wireframes.
+            // Can be changed per mesh using the `WireframeColor` component.
+            default_color: WHITE.into(),
+        })
+        .add_systems(Startup, startup)
+        .add_systems(Update, toggle_wireframe)
+        .run();
+}
 
-    //map.print();
+fn startup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    commands.spawn((
+        Camera3d { ..default() },
+        Transform::from_xyz(0.0, 20.0, 75.0).looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
+        PanOrbitCamera::default(),
+    ));
 
-    render2d(map);
+    let generated_map = generate_terrain_mesh(2048.0, 400, 80.0, 0.005, None);
+
+    commands.spawn((
+        Mesh3d(meshes.add(generated_map)),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Terrain,
+    ));
+
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::OVERCAST_DAY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+    ));
+}
+
+#[derive(Component)]
+struct Terrain;
+
+/// Quick way to toggle wireframes
+fn toggle_wireframe(
+    mut commands: Commands,
+    landscape_wireframes: Query<Entity, (With<Terrain>, With<Wireframe>)>,
+    landscapes: Query<Entity, (With<Terrain>, Without<Wireframe>)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        for terrain in &landscapes {
+            commands.entity(terrain).insert(Wireframe);
+        }
+        for terrain in &landscape_wireframes {
+            commands.entity(terrain).remove::<Wireframe>();
+        }
+    }
 }
